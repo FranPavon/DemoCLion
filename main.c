d754aa4e17854e47dcd8db758416c232f66ae488
Fran_Pavon <pavonfrancisco@gmail.com>
2023-03-10 11:43:54 -0300

    ADD - Primer commit

---
+++ main.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main() {
+    printf("Hello, World!\n");
+    //funcion que recibe un entero y calcula su raiz cuadrada
+    extern double sqrt(double);
+    double a = sqrt(2.0);
+    printf("La raiz cuadrada de 2 es: %f",a);
+
+    return 0;
+}
